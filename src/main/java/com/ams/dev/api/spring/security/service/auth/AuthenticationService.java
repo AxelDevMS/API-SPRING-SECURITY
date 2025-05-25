e240ac8d6f909a26eed85a345d64fcb706e6bd0d
AxelDevMS <asmtel2001@gmail.com>
2025-05-24 23:34:08 -0600

    feat:implements jwtservice

--- src/main/java/com/ams/dev/api/spring/security/service/auth/AuthenticationService.java
+++ src/main/java/com/ams/dev/api/spring/security/service/auth/AuthenticationService.java
@@ -6,6 +6,9 @@ import com.ams.dev.api.spring.security.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class AuthenticationService {
 
@@ -26,10 +29,18 @@ public class AuthenticationService {
         userRegister.setRole(user.getRole().name());
 
         //Generated Token
-        String jwt = jwtService.generateToken(user);
+        String jwt = jwtService.generateToken(user, generateExtraClaims(user));
         userRegister.setJwt(jwt);
 
 
         return userRegister;
+    }
+
+    private Map<String, Object> generateExtraClaims(UserEntity user) {
+        Map<String, Object> extraClaims = new HashMap<>();
+        extraClaims.put("name",user.getName());
+        extraClaims.put("role",user.getRole().name());
+        extraClaims.put("authorities",user.getAuthorities());
+        return extraClaims;
     }
 }
--- src/main/java/com/ams/dev/api/spring/security/service/auth/JwtService.java
+++ src/main/java/com/ams/dev/api/spring/security/service/auth/JwtService.java
@@ -1,13 +1,46 @@
 package com.ams.dev.api.spring.security.service.auth;
 
 import com.ams.dev.api.spring.security.persistence.entity.UserEntity;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JwtService {
 
-    public String generateToken(UserDetails user) {
-        return null;
+    @Value("${security.token.jwt.expiration-in-minutes}")
+    private Long EXPIRATION_IN_MINUTES;
+
+    @Value("${security.jwt.secret-key}")
+    private String SECRET_KEY;
+
+    public String generateToken(UserDetails user, Map<String,Object> extraClaims) {
+
+        Date issueAt = new Date(System.currentTimeMillis());
+        Date expiration = new Date( (EXPIRATION_IN_MINUTES * 60 * 1000)+issueAt.getTime());
+        String jwt = Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(issueAt)
+                .setExpiration(expiration)
+                .setHeaderParam(Header.TYPE,Header.JWT_TYPE)
+                .signWith(generateKey(), SignatureAlgorithm.HS256)
+                .compact();
+
+        return jwt;
+    }
+
+    private Key generateKey() {
+        byte[] key = SECRET_KEY.getBytes();
+        return Keys.hmacShaKeyFor(key);
     }
 }
