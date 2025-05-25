8267aa3586642b86c9ccb44f557ec707734c1f84
AxelDevMS <asmtel2001@gmail.com>
2025-05-24 18:05:08 -0600

    feat: create userDetailsService an PasswordEncoder

--- src/main/java/com/ams/dev/api/spring/security/config/security/SecurityBeansInjector.java
+++ src/main/java/com/ams/dev/api/spring/security/config/security/SecurityBeansInjector.java
@@ -1,5 +1,7 @@
 package com.ams.dev.api.spring.security.config.security;
 
+import com.ams.dev.api.spring.security.exception.ObjectNotFoundException;
+import com.ams.dev.api.spring.security.persistence.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,12 +9,18 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityBeansInjector {
 
     @Autowired
     private AuthenticationConfiguration authenticationConfiguration;
+
+    @Autowired
+    private UserRepository userRepository;
 
 
     @Bean
@@ -24,11 +32,24 @@ public class SecurityBeansInjector {
     public AuthenticationProvider authenticationProvider(){
 
         DaoAuthenticationProvider authenticationStrategy = new DaoAuthenticationProvider();
-        authenticationStrategy.setPasswordEncoder(null);
-        authenticationStrategy.setUserDetailsService(null);
+        authenticationStrategy.setPasswordEncoder(passwordEncoder());
+        authenticationStrategy.setUserDetailsService(userDetailsService());
 
         return authenticationStrategy;
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return (username) ->{
+            return userRepository.findByUsername(username).orElseThrow(()->new ObjectNotFoundException("User not found with username "+username));
+        } ;
+    }
+
 
 
 }
